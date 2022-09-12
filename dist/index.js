183676ba1e0b11fa8714c480b9f35aa4302adbb6
Your Name <you@example.com>
2022-09-12 17:12:53 +0700

    bt

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
+const fs_1 = __importDefault(require("fs"));
 const port = 3000;
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
@@ -16,6 +17,20 @@ app.get('/', (req, res) => {
     res.json({
         message: "Hello Stranger! How are you?"
     });
+});
+app.get('/one', (req, res, next) => {
+    fs_1.default.promises.readFile('./one.txt')
+        .then(data => res.send(data))
+        .catch(err => next(err));
+});
+app.use((err, req, res, next) => {
+    console.error('Error 1111: ', err.type);
+    if (err.type == 'time-out') {
+        res.status(408).send(err);
+    }
+    else {
+        res.status(500).send(err);
+    }
 });
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import helmet from "helmet";
+import fs from "fs";
 
 
 const port = 3000;
@@ -9,16 +10,30 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(morgan('common'));
-//helmet
 app.use(helmet());
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.json({
-        message : "Hello Stranger! How are you?"
+        message: "Hello Stranger! How are you?"
     });
 });
 
+app.get('/one', (req, res, next) => {
+    fs.promises.readFile('./one.txt')
+        .then(data => res.send(data))
+        .catch(err => next(err))
+});
+
+app.use((err, req, res, next)=>{
+    console.error('Error 1111: ', err.type);
+
+    if(err.type == 'time-out'){
+        res.status(408).send(err)
+    }else {
+        res.status(500).send(err)
+    }
+});
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 })
